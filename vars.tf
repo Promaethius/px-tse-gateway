0b70b46684447b4fee06f2017d5aca8e0793ac70
Promaethius <bryant.jonathan.42@gmail.com>
2021-05-18 14:34:26 -0600

    prometheus reloaders and operator

--- vars.tf
+++ vars.tf
@@ -57,7 +57,10 @@ variable "gateway" {
             csi-versions    = ["2.2.0"]
             extras          = [
                 "plndr/kube-vip:0.3.2",
-                "quay.io/coreos/prometheus-operator:v0.34.0"
+                "quay.io/coreos/prometheus-operator:v0.34.0",
+                "quay.io/coreos/prometheus-config-reloader:v0.34.0",
+                "quay.io/coreos/configmap-reload:v0.0.1",
+                "quay.io/prometheus/prometheus:v2.7.1"
             ]
         }
     }
